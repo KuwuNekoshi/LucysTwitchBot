4b3e498ca4db2946d9e38c48722d05b2abef22bc
Rasmus Rosenqvist <raro0907@hotmail.com>
2024-04-02 00:00:31 +0200

    adfasdf

--- token.js
+++ token.js
@@ -12,7 +12,26 @@ async function getToken() {
 }
 
 async function getBroadcasterId(channelName) {
-    return "217731363";
+    const oAuthToken = await getToken();
+    const url = `https://api.twitch.tv/helix/users?login=${channelName}`;
+    const headers = {
+        'Client-ID': clientId,
+        'Authorization': `Bearer ${oAuthToken}`,
+    };
+
+    try {
+        const response = await fetch(url, { headers });
+        const data = await response.json();
+        if (data.data && data.data.length > 0) {
+            return data.data[0].id;
+        } else {
+            console.log('User not found');
+            return null;
+        }
+    } catch (error) {
+        console.error('Error fetching broadcaster ID:', error);
+        return null;
+    }
 }
 
 async function saveTokens(accessToken, refreshToken) {
